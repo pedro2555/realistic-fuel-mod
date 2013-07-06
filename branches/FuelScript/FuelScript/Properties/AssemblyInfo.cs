8c2c40a0bd3ad84cf3b58c36756b9a69ba26aa47
SANDAKELUM2009@gmail.com <SANDAKELUM2009@gmail.com@ad1526db-8dfb-9cef-0cbd-ad1bf258eeb8>
2013-07-06 14:25:05 +0000

    Updated to 2.2.0.4 as 2.2.0.3 released to public.

--- branches/FuelScript/FuelScript/Properties/AssemblyInfo.cs
+++ branches/FuelScript/FuelScript/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
- using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fuel Mod by Sandakelum")]
-[assembly: AssemblyDescription("Fuel Mod by Sandakelum")]
+[assembly: AssemblyTitle("Realistic Fuel Mod")]
+[assembly: AssemblyDescription("Realistic Fuel Mod")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sandakelum (sandakelum2009@gmail.com)")]
+[assembly: AssemblyCompany("ScriptHook Script")]
 [assembly: AssemblyProduct("FuelScript")]
-[assembly: AssemblyCopyright("Copyright © 2013 Sandakelum. All Rights Reserved.")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2013 Realistic Fuel Mod. All Rights Reserved.")]
+[assembly: AssemblyTrademark("Copyright © 2013 Realistic Fuel Mod. All Rights Reserved.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.3")]
-[assembly: AssemblyFileVersion("2.2.0.3")]
+[assembly: AssemblyVersion("2.2.0.4")]
+[assembly: AssemblyFileVersion("2.2.0.4")]
