019abcf12870d8e71f39f349f8aecbb3de46abd4
prodrigues1990@gmail.com <prodrigues1990@gmail.com@ad1526db-8dfb-9cef-0cbd-ad1bf258eeb8>
2013-07-10 23:07:38 +0000

    DashboardPositioner Added width option. Added Save to ini file option. Moved controls to center screen.

--- branches/DashboardPositioner/DashboardPositioner/mainForm.Designer.cs
+++ branches/DashboardPositioner/DashboardPositioner/mainForm.Designer.cs
@@ -34,6 +34,13 @@
             this.lblDashboard = new System.Windows.Forms.Label();
             this.btnCopy = new System.Windows.Forms.Button();
             this.lblHelp = new System.Windows.Forms.Label();
+            this.nupWidth = new System.Windows.Forms.NumericUpDown();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblWidthText = new System.Windows.Forms.Label();
+            this.lblHelp1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.nupWidth)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblRes
@@ -41,7 +48,7 @@
             this.lblRes.AutoSize = true;
             this.lblRes.BackColor = System.Drawing.Color.Transparent;
             this.lblRes.ForeColor = System.Drawing.Color.Lime;
-            this.lblRes.Location = new System.Drawing.Point(12, 17);
+            this.lblRes.Location = new System.Drawing.Point(161, 12);
             this.lblRes.Name = "lblRes";
             this.lblRes.Size = new System.Drawing.Size(98, 13);
             this.lblRes.TabIndex = 0;
@@ -49,10 +56,11 @@
             // 
             // btnExit
             // 
-            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnExit.FlatAppearance.BorderSize = 0;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.ForeColor = System.Drawing.Color.Lime;
-            this.btnExit.Location = new System.Drawing.Point(810, 12);
+            this.btnExit.Location = new System.Drawing.Point(219, 111);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(44, 23);
             this.btnExit.TabIndex = 1;
@@ -66,7 +74,7 @@
             this.pnlDashboard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pnlDashboard.ForeColor = System.Drawing.Color.DarkGreen;
-            this.pnlDashboard.Location = new System.Drawing.Point(90, 167);
+            this.pnlDashboard.Location = new System.Drawing.Point(368, 300);
             this.pnlDashboard.Name = "pnlDashboard";
             this.pnlDashboard.Size = new System.Drawing.Size(131, 10);
             this.pnlDashboard.TabIndex = 2;
@@ -81,18 +89,19 @@
             this.lblDashboard.AutoSize = true;
             this.lblDashboard.BackColor = System.Drawing.Color.Transparent;
             this.lblDashboard.ForeColor = System.Drawing.Color.Lime;
-            this.lblDashboard.Location = new System.Drawing.Point(12, 57);
+            this.lblDashboard.Location = new System.Drawing.Point(36, 12);
             this.lblDashboard.Name = "lblDashboard";
-            this.lblDashboard.Size = new System.Drawing.Size(136, 13);
+            this.lblDashboard.Size = new System.Drawing.Size(105, 13);
             this.lblDashboard.TabIndex = 0;
-            this.lblDashboard.Text = "Dashboard location : x= y= ";
+            this.lblDashboard.Text = "Dashboard location :";
             // 
             // btnCopy
             // 
-            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnCopy.FlatAppearance.BorderSize = 0;
             this.btnCopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCopy.ForeColor = System.Drawing.Color.Lime;
-            this.btnCopy.Location = new System.Drawing.Point(696, 12);
+            this.btnCopy.Location = new System.Drawing.Point(31, 111);
             this.btnCopy.Name = "btnCopy";
             this.btnCopy.Size = new System.Drawing.Size(108, 23);
             this.btnCopy.TabIndex = 1;
@@ -105,11 +114,92 @@
             this.lblHelp.AutoSize = true;
             this.lblHelp.BackColor = System.Drawing.Color.Transparent;
             this.lblHelp.ForeColor = System.Drawing.Color.Gold;
-            this.lblHelp.Location = new System.Drawing.Point(58, 138);
+            this.lblHelp.Location = new System.Drawing.Point(50, 150);
             this.lblHelp.Name = "lblHelp";
             this.lblHelp.Size = new System.Drawing.Size(195, 13);
             this.lblHelp.TabIndex = 0;
             this.lblHelp.Text = "Move this around to the desired position";
+            // 
+            // nupWidth
+            // 
+            this.nupWidth.BackColor = System.Drawing.Color.Gray;
+            this.nupWidth.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nupWidth.DecimalPlaces = 3;
+            this.nupWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nupWidth.ForeColor = System.Drawing.Color.Lime;
+            this.nupWidth.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.nupWidth.Location = new System.Drawing.Point(140, 76);
+            this.nupWidth.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nupWidth.Name = "nupWidth";
+            this.nupWidth.Size = new System.Drawing.Size(65, 18);
+            this.nupWidth.TabIndex = 3;
+            this.nupWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nupWidth.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            131072});
+            this.nupWidth.ValueChanged += new System.EventHandler(this.nupWidth_ValueChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.Lime;
+            this.btnSave.Location = new System.Drawing.Point(145, 111);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(68, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save to .ini";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblWidthText
+            // 
+            this.lblWidthText.AutoSize = true;
+            this.lblWidthText.BackColor = System.Drawing.Color.Transparent;
+            this.lblWidthText.ForeColor = System.Drawing.Color.Lime;
+            this.lblWidthText.Location = new System.Drawing.Point(90, 77);
+            this.lblWidthText.Name = "lblWidthText";
+            this.lblWidthText.Size = new System.Drawing.Size(44, 13);
+            this.lblWidthText.TabIndex = 0;
+            this.lblWidthText.Text = "Width : ";
+            // 
+            // lblHelp1
+            // 
+            this.lblHelp1.AutoSize = true;
+            this.lblHelp1.BackColor = System.Drawing.Color.Transparent;
+            this.lblHelp1.ForeColor = System.Drawing.Color.Gold;
+            this.lblHelp1.Location = new System.Drawing.Point(78, 60);
+            this.lblHelp1.Name = "lblHelp1";
+            this.lblHelp1.Size = new System.Drawing.Size(139, 13);
+            this.lblHelp1.TabIndex = 0;
+            this.lblHelp1.Text = "Adjust the gauge width here";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.lblHelp);
+            this.panel1.Controls.Add(this.nupWidth);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.lblWidthText);
+            this.panel1.Controls.Add(this.btnCopy);
+            this.panel1.Controls.Add(this.lblHelp1);
+            this.panel1.Controls.Add(this.lblDashboard);
+            this.panel1.Controls.Add(this.lblRes);
+            this.panel1.Location = new System.Drawing.Point(286, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 170);
+            this.panel1.TabIndex = 4;
             // 
             // mainForm
             // 
@@ -119,11 +209,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(866, 434);
             this.Controls.Add(this.pnlDashboard);
-            this.Controls.Add(this.btnCopy);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.lblDashboard);
-            this.Controls.Add(this.lblRes);
-            this.Controls.Add(this.lblHelp);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -133,8 +219,10 @@
             this.Text = "Dashboard Position for Realistic Fuel Mod";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nupWidth)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -146,6 +234,11 @@
         private System.Windows.Forms.Label lblDashboard;
         private System.Windows.Forms.Button btnCopy;
         private System.Windows.Forms.Label lblHelp;
+        private System.Windows.Forms.NumericUpDown nupWidth;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblWidthText;
+        private System.Windows.Forms.Label lblHelp1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- branches/DashboardPositioner/DashboardPositioner/mainForm.cs
+++ branches/DashboardPositioner/DashboardPositioner/mainForm.cs
@@ -14,7 +14,7 @@ namespace DashboardPositioner
     public partial class mainForm : Form
     {
         private Point _offSet;
-        float x, y;
+        float x, y, w;
 
         public mainForm()
         {
@@ -33,8 +33,11 @@ namespace DashboardPositioner
         {
             this.lblRes.Text = String.Format("Current resolution :\n{0}x{1}", this.Width, this.Height);
             this.pnlDashboard_LocationChanged(this, e);
-
-            this.pnlDashboard.Width = (int)(11 * this.Width) / 100;
+            
+            w = .11f;
+            this.pnlDashboard.Width = (int)((w * 100) * this.Width) / 100;
+            this.panel1.Location = new Point((this.Width / 2) - (this.panel1.Width / 2), (this.Height / 2) - (this.panel1.Height / 2));
+            this.pnlDashboard.Location = new Point((this.Width / 2) - (this.pnlDashboard.Width / 2), (this.Height / 2) + (this.panel1.Height / 2));
         }
 
         private void pnlDashboard_LocationChanged(object sender, EventArgs e)
@@ -73,9 +76,9 @@ namespace DashboardPositioner
             x = (pnlDashboard.Location.X * 100.0f) / this.Width;
             y = (pnlDashboard.Location.Y * 100.0f) / this.Height;
 
-            Clipboard.SetText(String.Format("X={0}\nY={1}", x / 100.0f, y / 100.0f).Replace(',', '.'));
+            Clipboard.SetText(String.Format("X={0}\nY={1}\nW={2}", x / 100.0f, y / 100.0f, w).Replace(',', '.'));
 
-            MessageBox.Show(String.Format("X={0}\nY={1}", x / 100.0f, y / 100.0f).Replace(',', '.') + "\nCopied to clipboard. Press CTRL + V, to paste it.", "Position copied");
+            MessageBox.Show(String.Format("X={0}\nY={1}\nW={2}", x / 100.0f, y / 100.0f, w).Replace(',', '.') + "\n\nCopied to clipboard. Press CTRL + V, to paste it.", "Position copied");
         }
 
         private void pnlDashboard_Paint(object sender, PaintEventArgs e)
@@ -92,5 +95,77 @@ namespace DashboardPositioner
             }
         }
 
+        private void nupWidth_ValueChanged(object sender, EventArgs e)
+        {
+            lblHelp1.Visible = false;
+            w = (float)nupWidth.Value;
+            this.pnlDashboard.Width = (int)((w * 100) * this.Width) / 100;
+            this.pnlDashboard.Invalidate();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\FuelScript.ini";
+            if (File.Exists(path))
+            {
+                SaveToFile(path);
+            }
+            else
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "FuelScript.ini | FuelScript.ini";
+                dialog.CheckFileExists = true;
+                dialog.InitialDirectory = "C:\\";
+                dialog.Title = "Please indicate where your FuelScript.ini file is.";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    SaveToFile(dialog.FileName);
+            }
+        }
+
+
+        private void SaveToFile(string filePath)
+        {
+            StreamReader sr;
+            StreamWriter sw;
+            try
+            {
+                sr = new StreamReader(filePath);
+                List<string> lines = new List<string>();
+                try
+                {
+                    string line = "";
+                    do
+                    {
+                        line = sr.ReadLine();
+                        if (line.ToUpper().StartsWith("X="))
+                            lines.Add("X=" + x.ToString().Replace(',', '.'));
+                        else if (line.ToUpper().StartsWith("Y="))
+                            lines.Add("Y=" + y.ToString().Replace(',', '.'));
+                        else if (line.ToUpper().StartsWith("W="))
+                            lines.Add("W=" + w.ToString().Replace(',', '.'));
+                        else
+                            lines.Add(line);
+                    } while (line != null);
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    sr.Close();
+                }
+                sw = new StreamWriter(filePath);
+                foreach (string l in lines)
+                    sw.WriteLine(l);
+                sw.Close();
+
+                MessageBox.Show("Settings file updated.");
+            }
+            catch (Exception crap)
+            {
+                MessageBox.Show("Failed to update file.\n" + crap.Message);
+            }
+        }
     }
 }
