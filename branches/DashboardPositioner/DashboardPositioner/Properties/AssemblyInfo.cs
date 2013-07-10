da5158e69f7c13c3e9cde348459cd8dd6b7c5fc7
prodrigues1990@gmail.com <prodrigues1990@gmail.com@ad1526db-8dfb-9cef-0cbd-ad1bf258eeb8>
2013-07-10 21:25:12 +0000

    DashboardPositioner Publicly released.

--- branches/DashboardPositioner/DashboardPositioner/Properties/AssemblyInfo.cs
+++ branches/DashboardPositioner/DashboardPositioner/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DashboardPositioner")]
+[assembly: AssemblyTitle("DashboardPositioner for Realistic Fuel Mod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DashboardPositioner")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Realistic Fuel Mod")]
+[assembly: AssemblyCopyright("Public Domain Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.107")]
+[assembly: AssemblyFileVersion("1.0.0.107")]
