6caf94d4aa4fa7fb6d5201242dbd35082f997cab
prodrigues1990@gmail.com <prodrigues1990@gmail.com@ad1526db-8dfb-9cef-0cbd-ad1bf258eeb8>
2013-07-10 18:40:02 +0000

    Added an application to simplify the process of moving the dashboard location. Still under development.

---
+++ branches/DashboardPositioner/DashboardPositioner/mainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DashboardPositioner
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRes = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlDashboard = new System.Windows.Forms.Panel();
+            this.lblDashboard = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(12, 17);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(98, 13);
+            this.lblRes.TabIndex = 0;
+            this.lblRes.Text = "Current resolution : ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(821, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(33, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pnlDashboard
+            // 
+            this.pnlDashboard.BackColor = System.Drawing.Color.Navy;
+            this.pnlDashboard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDashboard.Location = new System.Drawing.Point(269, 193);
+            this.pnlDashboard.Name = "pnlDashboard";
+            this.pnlDashboard.Size = new System.Drawing.Size(97, 14);
+            this.pnlDashboard.TabIndex = 2;
+            this.pnlDashboard.LocationChanged += new System.EventHandler(this.pnlDashboard_LocationChanged);
+            this.pnlDashboard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlDashboard_MouseDown);
+            this.pnlDashboard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlDashboard_MouseMove);
+            this.pnlDashboard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlDashboard_MouseUp);
+            // 
+            // lblDashboard
+            // 
+            this.lblDashboard.AutoSize = true;
+            this.lblDashboard.Location = new System.Drawing.Point(12, 41);
+            this.lblDashboard.Name = "lblDashboard";
+            this.lblDashboard.Size = new System.Drawing.Size(136, 13);
+            this.lblDashboard.TabIndex = 0;
+            this.lblDashboard.Text = "Dashboard location : x= y= ";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(866, 434);
+            this.Controls.Add(this.pnlDashboard);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblDashboard);
+            this.Controls.Add(this.lblRes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel pnlDashboard;
+        private System.Windows.Forms.Label lblDashboard;
+    }
+}
+
---
+++ branches/DashboardPositioner/DashboardPositioner/mainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DashboardPositioner
+{
+    public partial class mainForm : Form
+    {
+        private Point _offSet;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            this.Icon = new System.Drawing.Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("DashboardPositioner.GTAIV.ico"));
+            this.lblRes.Text = String.Format("Current resolution : {0}x{1}", this.Width, this.Height);
+            _offSet = Point.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            this.lblRes.Text = String.Format("Current resolution : {0}x{1}", this.Width, this.Height);
+        }
+
+        private void pnlDashboard_LocationChanged(object sender, EventArgs e)
+        {
+            float x, y;
+            x = (pnlDashboard.Location.X * 100) / this.Width;
+            y = (pnlDashboard.Location.Y * 100) / this.Height;
+            this.lblDashboard.Text = String.Format("Dashboard location : x={0} y={1}", x / 100, y / 100);
+        }
+
+        private void pnlDashboard_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                _offSet = new Point(e.X, e.Y);
+        }
+
+        private void pnlDashboard_MouseUp(object sender, MouseEventArgs e)
+        {
+            _offSet = Point.Empty;
+        }
+
+        private void pnlDashboard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_offSet != Point.Empty)
+            {
+                Point newlocation = this.pnlDashboard.Location;
+                newlocation.X += e.X - _offSet.X;
+                newlocation.Y += e.Y - _offSet.Y;
+                this.pnlDashboard.Location = newlocation;
+            }
+        }
+
+    }
+}
